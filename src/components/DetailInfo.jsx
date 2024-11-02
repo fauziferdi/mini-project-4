6b0d0fdd2532ec4b707693fa3ffeb0ef366de1ae
fauziferdi <fauziferdiansyahindo@gmail.com>
2024-11-03 00:22:50 +0700

    final commit

--- src/components/DetailInfo.jsx
+++ src/components/DetailInfo.jsx
@@ -15,30 +15,36 @@ export default function DetailInfo({ siswa, toggleDetailModal }) {
           ></button>
         </div>
         <div className="modal-body">
-          <p>
-            <strong>Name : </strong> {siswa.name}
-          </p>
-          <p>
-            <strong>NIM : </strong> {siswa.nim}
-          </p>
-          <p>
-            <strong>Class : </strong> {siswa.class}
-          </p>
-          <p>
-            <strong>Year : </strong> {siswa.year}
-          </p>
-          <p>
-            <strong>Guardian Name : </strong> {siswa.guardian_name}
-          </p>
-          <p>
-            <strong>Birth Date : </strong> {siswa.birthDate}
-          </p>
-          <p>
-            <strong>Address : </strong> {siswa.address}
-          </p>
-          <p>
-            <strong>Gender : </strong> {siswa.gender}
-          </p>
+          <div className="row">
+            <div className="col-md-6">
+              <p>
+                <strong>Name : </strong> {siswa.name}
+              </p>
+              <p>
+                <strong>NIM : </strong> {siswa.nim}
+              </p>
+              <p>
+                <strong>Class : </strong> {siswa.class}
+              </p>
+              <p>
+                <strong>Year : </strong> {siswa.year}
+              </p>
+            </div>
+            <div className="col-md-6">
+              <p>
+                <strong>Guardian Name : </strong> {siswa.guardian_name}
+              </p>
+              <p>
+                <strong>Birth Date : </strong> {siswa.birthDate}
+              </p>
+              <p>
+                <strong>Address : </strong> {siswa.address}
+              </p>
+              <p>
+                <strong>Gender : </strong> {siswa.gender}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
